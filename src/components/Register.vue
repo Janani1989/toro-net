19eb3b24effc09ebbdd5636ccea61dedeba82acd
sthakur2 <sahil.thakur16@gmail.com>
2017-10-16 11:17:29 -0700

    Update Form Validation

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -6,13 +6,25 @@
    
       <input type="text" class="form-control" placeholder="Display name" v-model="displayName" name="displayName">
     </div>
-    <div class="form-group">
-      <input type="text" class="form-control" placeholder="Username" v-model="username" name="username">
-    </div>
+    <div class="form-group" :class="{'has-error': errors.has('username') }" >
+          <input v-model="username" name="username" v-validate="'required|alpha|min:4'" data-vv-delay="500" type="text" data-vv-as="username" placeholder="UserName" class="form-control">
+          <p class="text-danger" align="left" v-if="errors.has('username')">{{ errors.first('username') }}</p>
+</div>
+</div>
     <div class="form-group" :class="{'has-error': errors.has('email') }" >
           <input v-model="email" name="email" v-validate="'required|email'" data-vv-delay="500" type="text" data-vv-as="email address" placeholder="Email" class="form-control">
           <p class="text-danger" align="left" v-if="errors.has('email')">{{ errors.first('email') }}</p>
 </div>
+        <div class="form-group"><div class="question">Question: When you were young, what did you want to be when you grew up?</div>
+      <input type="text" class="form-control" placeholder="Answer" v-model="question1" name="question1">
+    </div>
+    <div class="form-group"><div class="question">Question: Who was your childhood hero?</div>
+      <input type="text" class="form-control" placeholder="Answer" v-model="question2" name="question2">
+    </div>
+    <div class="form-group"><div class="question">Question: What was the last name of your favorite teacher?</div>
+      <input type="text" class="form-control" placeholder="Answer" v-model="question3" name="question3">
+    </div>
+
     <div class="form-group">
       <input type="password" class="form-control" placeholder="Password" v-model="password" name="password">
     </div>
@@ -23,6 +35,20 @@
     </form>
   </div>
 </template>
+
+<style scoped>
+.question {
+  border: 0;
+  -background: #444;
+  color: black;
+  text-align: left;
+  
+}
+.btn-github:hover {
+  background: #2B2B2B;
+  color: white;
+}
+</style>
 
 <script>
 export default {
